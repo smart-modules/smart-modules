2b47f24b7a6a75e0ca3ce711b96dca0d6f735efe
Anand Suresh <anand.suresh@voxer.com>
2019-11-29 17:48:20 -0800

    queue: initial commit of smart-queue

---
+++ modules/queue/lib/queue.js
@@ -0,0 +1,165 @@
+/**
+ * @file Implements a smart-queue
+ */
+'use strict'
+
+const { EventEmitter } = require('events')
+
+/**
+ * A list of strategies when enqueuing on a full queue
+ * @type {Array}
+ * @private
+ */
+const STRATEGIES = ['drop', 'overwrite', 'grow']
+
+/**
+ * Default properties for the constructor
+ * @type {Object}
+ * @private
+ */
+const DEFAULT_PROPS = {
+  strategy: 'grow',
+  size: Infinity
+}
+
+/**
+ * Implements a queue that supports multiple strategies for enqueueing elements
+ * when the queue is full. The strategies include:
+ *
+ * - `drop`: drops new data items when the queue is full; `.enqueue()` returns
+ * false
+ * - `overwrite`: overwrites old data items when the queue is full; `.enqueue()`
+ * returns true
+ * - `grow`: grows the size of the queue, when full, to accommodate new data
+ * items; `.enqueue()` always returns true, enqueuing new items until the
+ * process runs out of memory.
+ */
+class SmartQueue extends EventEmitter {
+  /**
+   * Constructs a `SmartQueue` instance
+   * @param {Object} [props] Properties of the queue
+   * @param {String} [props.strategy='grow'] The enqueuing strategy to use
+   * @param {Number} [props.size=Infinity] The size of the queue
+   * @returns {SmartQueue}
+   */
+  constructor (props) {
+    let { strategy, size } = props = Object.assign({}, DEFAULT_PROPS, props)
+
+    if (!SmartQueue.STRATEGIES.includes(strategy)) {
+      throw new TypeError(`unknown strategy "${strategy}"!`)
+    }
+
+    if (strategy === 'grow') {
+      size = Infinity
+    } else if (size == null || isNaN(size) || size <= 0 || size >= Infinity) {
+      throw new TypeError(`invalid size (${size})!`)
+    }
+
+    super()
+
+    this._strategy = strategy
+    this._size = size
+    this._length = 0
+    this._data = (this._size === Infinity) ? [] : new Array(this._size)
+    this._head = 0
+    this._tail = -1
+  }
+
+  /**
+   * Returns the enqueuing strategy being used by the quque
+   * @type {String}
+   */
+  get strategy () {
+    return this._strategy
+  }
+
+  /**
+   * Returns the size of the queue
+   * @type {Number}
+   */
+  get size () {
+    return this._size
+  }
+
+  /**
+   * Returns the number of data items enqueued in the queue
+   * @type {Number}
+   */
+  get length () {
+    return this._length
+  }
+
+  /**
+   * Enqueues a single data item in the queue
+   * @param {*} data The data to be enqueued
+   * @returns {Boolean} true, if the data was successfully enqueued; false
+   * otherwise
+   */
+  enqueue (data) {
+    if (this._length < this._size) {
+      this._data[this._head] = data
+      this._head = (this._head + 1) % this._size
+      this._length++
+      return true
+    }
+
+    switch (this.strategy) {
+      case 'drop':
+        return false
+
+      case 'overwrite':
+        this._data[this._head] = data
+        this._head = (this._head + 1) % this._size
+        this._tail = (this._tail + 1) % this._size
+        return true
+
+      /* istanbul ignore next */
+      case 'grow': // For sake of clarity/completeness
+        throw new Error('this is really really bad! all bets are off!')
+
+      /* istanbul ignore next */
+      default:
+        throw new TypeError(`unknown strategy "${this.strategy}"`)
+    }
+  }
+
+  /**
+   * Dequeues a single data item from the queue
+   * @returns {*|undefined} The data that was dequeue'd, if any; else undefined
+   */
+  dequeue () {
+    if (this._length > 0) {
+      this._tail = (this._tail + 1) % this._size
+      const data = this._data[this._tail]
+      this._data[this._tail] = undefined
+      this._length--
+      return data
+    }
+
+    throw new Error('Attempted to dequeue on an empty queue!')
+  }
+
+  /**
+   * A list of strategies for enqueuing new data when the queue is full.
+   * @type {Array}
+   * @enum
+   */
+  static get STRATEGIES () {
+    return STRATEGIES
+  }
+
+  /**
+   * Functional form of instantiating a SmartQueue
+   * @param {Object} [props] Properties of the queue; @see {SmartQueue}
+   * @returns {SmartQueue}
+   */
+  static create (props) {
+    return new SmartQueue(props)
+  }
+}
+
+/**
+ * Export the interface
+ * @type {SmartQueue}
+ */
+module.exports = SmartQueue
---
+++ modules/queue/spec/queue.spec.js
@@ -0,0 +1,130 @@
+/**
+ * @file Unit tests for the SmartQueue
+ */
+'use strict'
+
+const { expect } = require('chai')
+const SmartQueue = require('../lib/queue')
+
+describe('SmartQueue', () => {
+  describe('instantiation', () => {
+    it('must not be instantiable with invalid arguments', function () {
+      [
+        [{ strategy: 'unknown' }],
+        [{ strategy: 'drop' }],
+        [{ strategy: 'overwrite' }]
+      ].forEach(args => {
+        expect(() => new SmartQueue(...args)).to.throw()
+      })
+    })
+
+    it('must be instantiable with valid arguments', () => {
+      [
+        [],
+        [null],
+        [{}],
+        [{ strategy: 'drop', size: 10 }],
+        [{ strategy: 'overwrite', size: 10 }]
+      ].forEach(args => {
+        expect(() => new SmartQueue(...args)).to.not.throw()
+      })
+    })
+
+    it('must be instantiable the functional way', function () {
+      let queue = null
+      expect(() => { queue = SmartQueue.create() }).to.not.throw()
+
+      expect(queue.strategy).to.equal('grow')
+      expect(queue.size).to.equal(Infinity)
+      expect(queue.length).to.equal(0)
+    })
+  })
+
+  describe('operation', () => {
+    describe('strategy: drop', () => {
+      it('must enqueue new data when space is available', () => {
+        const queue = SmartQueue.create({ size: 1, strategy: 'drop' })
+        expect(queue.enqueue({ foo: 'bar' })).to.equal(true)
+        expect(queue.length).to.equal(1)
+      })
+
+      it('must not enqueue new data when space is not available', () => {
+        const queue = SmartQueue.create({ size: 1, strategy: 'drop' })
+        expect(queue.enqueue({ foo: 'bar' })).to.equal(true)
+        expect(queue.enqueue({ bar: 'baz' })).to.equal(false)
+        expect(queue.length).to.equal(1)
+      })
+
+      it('must dequeue data when available', () => {
+        const queue = SmartQueue.create({ size: 1, strategy: 'drop' })
+        expect(queue.enqueue({ foo: 'bar' })).to.equal(true)
+        expect(queue.dequeue()).to.deep.equal({ foo: 'bar' })
+        expect(queue.length).to.equal(0)
+      })
+
+      it('must not dequeue data when not available', () => {
+        const queue = SmartQueue.create({ size: 1, strategy: 'drop' })
+        expect(queue.enqueue({ foo: 'bar' })).to.equal(true)
+        expect(queue.dequeue()).to.deep.equal({ foo: 'bar' })
+        expect(() => queue.dequeue()).to.throw()
+        expect(queue.length).to.equal(0)
+      })
+    })
+
+    describe('strategy: overwrite', () => {
+      it('must enqueue new data when space is available', () => {
+        const queue = SmartQueue.create({ size: 1, strategy: 'overwrite' })
+        expect(queue.enqueue({ foo: 'bar' })).to.equal(true)
+        expect(queue.length).to.equal(1)
+      })
+
+      it('must enqueue new data when space is not available', () => {
+        const queue = SmartQueue.create({ size: 2, strategy: 'overwrite' })
+        expect(queue.enqueue({ foo: 'bar' })).to.equal(true)
+        expect(queue.length).to.equal(1)
+        expect(queue.enqueue({ bar: 'baz' })).to.equal(true)
+        expect(queue.length).to.equal(2)
+        expect(queue.enqueue({ baz: 'foo' })).to.equal(true)
+        expect(queue.length).to.equal(2)
+
+        expect(queue.dequeue()).to.deep.equal({ bar: 'baz' })
+        expect(queue.length).to.equal(1)
+        expect(queue.dequeue()).to.deep.equal({ baz: 'foo' })
+        expect(queue.length).to.equal(0)
+      })
+
+      it('must dequeue data when available', () => {
+        const queue = SmartQueue.create({ size: 1, strategy: 'overwrite' })
+        expect(queue.enqueue({ foo: 'bar' })).to.equal(true)
+        expect(queue.dequeue()).to.deep.equal({ foo: 'bar' })
+        expect(queue.length).to.equal(0)
+      })
+
+      it('must not dequeue data when not available', () => {
+        const queue = SmartQueue.create({ size: 1, strategy: 'overwrite' })
+        expect(queue.enqueue({ foo: 'bar' })).to.equal(true)
+        expect(queue.dequeue()).to.deep.equal({ foo: 'bar' })
+        expect(() => queue.dequeue()).to.throw()
+        expect(queue.length).to.equal(0)
+      })
+    })
+
+    describe('strategy: grow', () => {
+      const queue = SmartQueue.create()
+
+      it('must enqueue data as long as there is memory available (64Mb heap usage)', function (done) {
+        ;(function enqueue (count) {
+          if (process.memoryUsage().heapUsed > 1024 * 1024 * 64) {
+            done()
+          } else {
+            if (++count % 1000 !== 0) {
+              queue.enqueue(Math.random()) && enqueue(count)
+            } else {
+              setImmediate(() => enqueue(count))
+            }
+          }
+        }(0))
+      })
+    })
+  })
+})
