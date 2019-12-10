522de20a486fd9ec19e55a96f2430c8ce4ee7af6
Anand Suresh <anand.suresh@voxer.com>
2019-12-09 22:05:36 -0800

    feat: added smart-timer module

---
+++ modules/timer/lib/timer.js
@@ -0,0 +1,147 @@
+/**
+ * @file Implements a smart-timer
+ */
+'use strict'
+
+const { EventEmitter } = require('events')
+
+/**
+ * Default properties for the constructor
+ * @type {Object}
+ * @private
+ */
+const DEFAULT_PROPS = {
+  timeout: 30000,
+  interval: 1000
+}
+
+/**
+ * Implements a smart-timer that checks for timeout at the specified inverval,
+ * calling the optional timeout handler when the timeout occurs.
+ * @extends {EventEmitter}
+ */
+class SmartTimer extends EventEmitter {
+  /**
+   * Constructs a `SmartTimer` instance and starts it immediately.
+   * @param {Object} [props] Properties of the timer
+   * @param {Number} [props.timeout=30000] The timeout for the timer (in ms)
+   * @param {Number} [props.interval=1000] Interval between checks for timeouts (in ms)
+   * @param {Function} [callback] Optional timeout event handler
+   * @returns {SmartTimer}
+   */
+  constructor (props, callback) {
+    if (typeof props === 'function') {
+      callback = props
+      props = DEFAULT_PROPS
+    } else if (props != null) {
+      if (+props.timeout < 0) {
+        throw new TypeError(`"${props.timeout}" is an invalid timeout!`)
+      } else if (+props.interval < 0) {
+        throw new TypeError(`"${props.interval}" is an invalid interval!`)
+      }
+    } else {
+      props = DEFAULT_PROPS
+    }
+
+    const { timeout, interval } = props = Object.assign({}, DEFAULT_PROPS, props)
+    if (interval > timeout) {
+      throw new Error(`interval (${interval}) exceeds timeout (${timeout})!`)
+    }
+
+    super()
+
+    this.timeout = timeout
+    this.interval = interval
+    this._timeoutTimer = setTimeout(() => this._onTimeout(), timeout)
+    this._intervalTimer = setInterval(() => this._onInterval(), interval)
+    this._hadActivity = false
+    this._lastActivity = Date.now()
+    this._destroyed = false
+
+    if (typeof callback === 'function') {
+      this.once('timeout', callback)
+    }
+  }
+
+  /**
+   * Handles the interval timer
+   * @private
+   */
+  _onInterval () {
+    if (this._hadActivity) {
+      this._lastActivity = Date.now()
+      this._hadActivity = false
+    }
+  }
+
+  /**
+   * Handles the timeout timer
+   * @private
+   */
+  _onTimeout () {
+    // Call this._onInterval() to account for any activity that might have
+    // occurred since the last interval check and the timeout.
+    this._onInterval()
+
+    const timeSinceLastActivity = Date.now() - this._lastActivity
+    if (timeSinceLastActivity < this.timeout) {
+      const timeout = this.timeout - timeSinceLastActivity
+      /* istanbul ignore next */
+      const onTimeout = () => this._onTimeout()
+      this._timeoutTimer = setTimeout(onTimeout, timeout)
+    } else {
+      this.destroy()
+      this.emit('timeout', timeSinceLastActivity)
+    }
+  }
+
+  /**
+   * Touches the timer to indicate activity.
+   * @throws {Error} The timer has already been destroyed.
+   */
+  touch () {
+    if (!this._destroyed) {
+      this._hadActivity = true
+    } else {
+      throw new Error('timer has already been destroyed!')
+    }
+  }
+
+  /**
+   * Destroys the timer instance.
+   */
+  destroy () {
+    /* istanbul ignore else  */
+    if (!this.destroyed) {
+      if (this._timeoutTimer !== null) {
+        clearTimeout(this._timeoutTimer)
+        this._timeoutTimer = null
+      }
+
+      if (this._intervalTimer !== null) {
+        clearInterval(this._intervalTimer)
+        this._intervalTimer = null
+      }
+
+      this._destroyed = true
+    }
+  }
+
+  /**
+   * Functional form of instantiating a SmartTimer
+   * @param {Object} [props] Properties of the timer
+   * @param {Number} [props.timeout=30000] The timeout for the timer (ms)
+   * @param {Number} [props.interval=1000] Interval between checks for timeouts (ms)
+   * @param {Function} [callback] Timeout event handler
+   * @returns {SmartTimer}
+   */
+  static create (props, callback) {
+    return new SmartTimer(props, callback)
+  }
+}
+
+/**
+ * Export the class
+ * @type {SmartTimer}
+ */
+module.exports = SmartTimer
---
+++ modules/timer/spec/timer.spec.js
@@ -0,0 +1,107 @@
+/**
+ * @file Unit tests for the SmartTimer
+ */
+'use strict'
+
+const { expect } = require('chai')
+const SmartTimer = require('../lib/timer')
+
+describe('SmartTimer', () => {
+  describe('instantiation', () => {
+    it('must not be instantiable with invalid arguments', function () {
+      [
+        [{ timeout: -2000, interval: 2000 }, () => {}],
+        [{ timeout: 2000, interval: -2000 }, () => {}],
+        [{ timeout: 1000, interval: 2000 }, () => {}]
+      ].forEach(args => {
+        expect(() => new SmartTimer(...args)).to.throw()
+      })
+    })
+
+    it('must be instantiable with valid arguments', () => {
+      [
+        [],
+        [null],
+        [{}],
+        [{ timeout: 2000 }],
+        [{ interval: 2000 }],
+        [],
+        [() => {}],
+        [{ timeout: 2000, interval: 2000 }, () => {}]
+      ].forEach(args => {
+        let timer = null
+        expect(() => { timer = new SmartTimer(...args) }).to.not.throw()
+        timer.destroy()
+      })
+    })
+
+    it('must be instantiable the functional way', function () {
+      expect(SmartTimer.create).to.be.a('function')
+
+      const args = { timeout: 500, interval: 100 }
+      let timer = null
+      expect(() => { timer = SmartTimer.create(args) }).to.not.throw()
+
+      expect(timer.timeout).to.equal(args.timeout)
+      expect(timer.interval).to.equal(args.interval)
+    })
+  })
+
+  describe('operation', () => {
+    it('must emit a `timeout` event when the timer times out', (done) => {
+      const props = { timeout: 500, interval: 100 }
+      SmartTimer.create(props)
+        .once('timeout', duration => {
+          expect(duration).to.be.at.least(props.timeout)
+          done()
+        })
+    })
+
+    it('must execute the default timeout callback', (done) => {
+      const props = { timeout: 500, interval: 100 }
+      const onTimeout = duration => {
+        expect(duration).to.be.at.least(props.timeout)
+        done()
+      }
+      SmartTimer.create(props, onTimeout) // eslint-disable-line no-new
+    })
+
+    it('must record activity correctly', done => {
+      const props = { timeout: 500, interval: 100 }
+      const onTimeout = () => done(new Error('should not have timed out!'))
+      const timer = SmartTimer.create(props, onTimeout)
+      const interval = setInterval(() => timer.touch(), 95)
+
+      setTimeout(function () {
+        clearInterval(interval)
+        timer.destroy()
+        done()
+      }, 1000)
+    })
+
+    it('must destroy the timer correctly', done => {
+      let destroyed = false
+      const props = { timeout: 500, interval: 100 }
+      const onTimeout = () => done(new Error('should not have timed out!'))
+      const timer = SmartTimer.create(props, onTimeout)
+      const interval = setInterval(function () {
+        try {
+          timer.touch()
+        } catch (e) {
+          clearInterval(interval)
+
+          if (destroyed) {
+            done()
+          } else {
+            done(new Error('timer not destroyed properly!'))
+          }
+        }
+      }, 100)
+
+      setTimeout(function () {
+        timer.destroy()
+        destroyed = true
+      }, 1000)
+    })
+  })
+})
